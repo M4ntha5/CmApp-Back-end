38cd9d2dcf82f10bb29cb628ae4da2e8e190423f
M4nthas <mantas.daunoravicius@ktu.edu>
2020-02-11 21:14:30 +0200

    tests added

---
+++ CmApp/Tests/ScraperTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NSubstitute;
+using CmApp.Domains;
+using CmApp.Services;
+
+namespace Tests
+{
+    public class ScraperTests
+    {
+        string Vin = string.Empty;
+        [SetUp]
+        public void Setup()
+        {
+            Vin = "wba3b1g58ens79736";
+        }
+
+        [Test]
+        public void TestGetVehicleInfo()
+        {
+            var scraper = new WebScraper();
+            var equipment = scraper.GetVehicleInfo(Vin);
+
+            Assert.AreNotEqual(null, equipment);
+        }
+
+        [Test]
+        public void TestGetVehicleEquipment()
+        {
+            var scraper = new WebScraper();
+            var equipment = scraper.GetVehicleEquipment(Vin);
+
+            Assert.AreNotEqual(null, equipment);
+        }
+    }
+}
