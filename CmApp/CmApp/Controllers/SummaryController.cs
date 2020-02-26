0f21bb8e12b2a0bc039f4d7d7642a878f1602c2d
M4nthas <mantas.daunoravicius@ktu.edu>
2020-02-26 20:38:10 +0200

    summary entity added

---
+++ CmApp/CmApp/Controllers/SummaryController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CmApp.Entities;
+using CmApp.Repositories;
+using CmApp.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmApp.Controllers
+{
+    [Route("/api/cars/{carId}/summary")]
+    [ApiController]
+    public class SummaryController : ControllerBase
+    {
+        private readonly SummaryService summaryService = new SummaryService()
+        {
+            SummaryRepository = new SummaryRepository()
+        };
+
+        // GET: /api/cars/{carId}/summary
+        [HttpGet]
+        public Task<SummaryEntity> Get(string carId)
+        {
+            var summary = summaryService.GetSummaryByCarId(carId);
+            return summary;
+        }
+
+        // POST: /api/cars/{carId}/summary
+        [HttpPost]
+        public SummaryEntity Post(string carId, [FromBody] SummaryEntity summary)
+        {
+            var newSummary = summaryService.InsertCarSummary(carId, summary).Result;
+            return newSummary;
+        }
+
+        // PUT: /api/cars/{carId}/summary/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(string carId, string summaryId, [FromBody] SummaryEntity summary)
+        {
+            await summaryService.UpdateSummary(carId, summaryId, summary);
+            return NoContent();
+        }
+
+        // DELETE: /api/cars/{carId}/summary/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string carid, string summaryId)
+        {
+            await summaryService.DeleteSummary(carid, summaryId);
+            return NoContent();
+        }
+    }
+}
---
+++ CmApp/CmApp/Entities/SummaryEntity.cs
@@ -0,0 +1,25 @@
+﻿using CodeMash.Models;
+using System;
+using System.Collections.Generic;
+
+
+namespace CmApp.Entities
+{
+    [Collection("Summary")]
+    public class SummaryEntity : Entity
+    {
+        [Field("bought_price")]
+        public double BoughtPrice { get; set; }
+        [Field("sold_price")]
+        public double SoldPrice { get; set; }
+        [Field("sold_date")]
+        public DateTime SoldDate { get; set; }
+        [Field("sold")]
+        public bool Sold { get; set; }
+        [Field("car")]
+        public string Car { get; set; }
+        [Field("transportation")]
+        public List<object> TransportationFile { get; set; }
+
+    }
+}
