bb14f1f7118971da47b65d86aafc946e0afb2f3e
Mantas Daunoravičius <36547991+M4ntha5@users.noreply.github.com>
2020-02-28 10:55:30 +0200

    summary service added

---
+++ BackEnd/CmApp/Repositories/SummaryRepository.cs
@@ -0,0 +1,69 @@
+﻿using CmApp.Contracts;
+using CmApp.Entities;
+using CmApp.Utils;
+using CodeMash.Client;
+using CodeMash.Repository;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CmApp.Repositories
+{
+    public class SummaryRepository : ISummaryRepository
+    {
+        private static CodeMashClient Client => new CodeMashClient(Settings.ApiKey, Settings.ProjectId);
+
+        public async Task<SummaryEntity> InsertSummary(SummaryEntity summary)
+        {
+            if (summary == null)
+            {
+                throw new ArgumentNullException(nameof(summary), "Cannot insert summary in db, because summary is empty");
+            }
+
+            var repo = new CodeMashRepository<SummaryEntity>(Client);
+
+            var response = await repo.InsertOneAsync(summary, new DatabaseInsertOneOptions());
+            return response;
+        }
+
+        public async Task<SummaryEntity> GetSummaryByCarId(string carId)
+        {
+            var repo = new CodeMashRepository<SummaryEntity>(Client);
+
+            var filter = Builders<SummaryEntity>.Filter.Eq("car", BsonObjectId.Create(carId));
+
+            var summary = await repo.FindOneAsync(filter, new DatabaseFindOneOptions());
+
+            return summary;
+        }
+
+        public async Task DeleteCarSummary(string carId, string summaryId)
+        {
+            var repo = new CodeMashRepository<SummaryEntity>(Client);
+
+            FilterDefinition<SummaryEntity>[] filters =
+            {
+                Builders<SummaryEntity>.Filter.Eq("car",  BsonObjectId.Create(carId)),
+                Builders<SummaryEntity>.Filter.Eq("_id",  BsonObjectId.Create(summaryId))
+            };
+
+            var filter = Builders<SummaryEntity>.Filter.And(filters);
+
+            await repo.DeleteOneAsync(filter);
+        }
+
+        public async Task UpdateCarSummary(SummaryEntity summary)
+        {
+            var repo = new CodeMashRepository<SummaryEntity>(Client);
+
+            await repo.ReplaceOneAsync(
+                x => x.Id == summary.Id,
+                summary,
+                new DatabaseReplaceOneOptions()
+            );
+        }
+    }
+}
